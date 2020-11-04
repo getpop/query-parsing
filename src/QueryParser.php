4ba83941d85892c8e52070947b8e384ebdaf0254
Leonardo Losoviz <leo@getpop.org>
2020-11-04 13:48:41 +0800

    Fixed phpstan level 8

--- src/QueryParser.php
+++ src/QueryParser.php
@@ -11,15 +11,19 @@ class QueryParser implements QueryParserInterface
      * is also inside the fieldArgs (i.e. inside the brackets "(" and ")")
      * Eg 1: Split elements by "|": ?query=id|posts(limit:3,order:title|ASC)
      * Eg 2: Split elements by ",": ?query=id,posts(ids:1175,1152).id|title
-     * Adapted from https://stackoverflow.com/a/1084924
+     * @param string[]|string|null $skipFromChars
+     * @param string[]|string|null $skipUntilChars
+     * @param array<string,mixed> $options
+     * @return string[]
+     * @see https://stackoverflow.com/a/1084924
      */
     public function splitElements(
         string $query,
         string $separator = ',',
         $skipFromChars = '(',
         $skipUntilChars = ')',
-        $ignoreSkippingFromChar = null,
-        $ignoreSkippingUntilChar = null,
+        ?string $ignoreSkippingFromChar = null,
+        ?string $ignoreSkippingUntilChar = null,
         array $options = []
     ): array {
         $buffer = '';
@@ -84,7 +88,7 @@ class QueryParser implements QueryParserInterface
                     // (eg: opening then closing quotes for strings)
                     // If it is not there, then treat this char as a normal char
                     // Eg: search:with some quote " is ok
-                    $restStrIgnoreSkippingUntilCharPos = strpos($query, $ignoreSkippingUntilChar, $charPos + 1);
+                    $restStrIgnoreSkippingUntilCharPos = strpos($query, (string) $ignoreSkippingUntilChar, $charPos + 1);
                     if ($restStrIgnoreSkippingUntilCharPos !== false) {
                         // Add this stretch of string into the buffer
                         $buffer .= substr($query, $charPos, $restStrIgnoreSkippingUntilCharPos - $charPos + 1);
@@ -115,12 +119,12 @@ class QueryParser implements QueryParserInterface
                         // inside the search are ignored):
                         // /api/?query=posts(searchfor:(.)).id|title
                         $restStr = substr($query, $charPos + 1);
-                        $restStrEndBracketPos = strpos($restStr, $skipUntilChars[0]);
+                        $restStrEndBracketPos = strpos($restStr, (string) $skipUntilChars[0]);
                         $restStrSeparatorPos = strpos($restStr, $separator);
                         if ($restStrEndBracketPos === false
                             || (
-                                $restStrSeparatorPos >= 0
-                                && $restStrEndBracketPos >= 0
+                                $restStrSeparatorPos !== false
+                                && $restStrEndBracketPos !== false
                                 && $restStrEndBracketPos > $restStrSeparatorPos
                             )
                         ) {
